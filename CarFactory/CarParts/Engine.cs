c7f67d78abaab30da8e04bb9b617e8a493915e92
Kriszitán Kovács <kovacsk88@gmail.com>
2019-12-02 12:29:13 +0100

    interface carpartok

--- CarFactory/CarParts/Engine.cs
+++ CarFactory/CarParts/Engine.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarFactory
+namespace CarFactory.CarParts
 {
-    class Program
+    class Engine
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
---
+++ CarFactory/Factory.cs
@@ -0,0 +1,26 @@
+﻿using CarFactory.CarParts;
+using CarFactory.FactoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    class Factory
+    {
+        private Car car;
+        private Chassis chassis;
+        private Body body;
+        private Engine engine;
+
+        private AssemblyPlant assemblyPlant;
+
+        public void SetAssemblyPlant(AssemblyPlant ap)
+        {
+            assemblyPlant = ap;
+        }
+
+    }
+}
