dd435d3c5e85fed47dca143471c0e83a548ad41e
Kriszitán Kovács <kovacsk88@gmail.com>
2019-12-02 13:59:10 +0100

    platnts rendberakás

--- CarFactory/Plants/AssemblyPlant.cs
+++ CarFactory/Plants/AssemblyPlant.cs
@@ -1,4 +1,7 @@
 ﻿using CarFactory.CarParts;
+using CarFactory.CarParts.Bodies;
+using CarFactory.CarParts.Chassies;
+using CarFactory.CarParts.Engines;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +13,23 @@ namespace CarFactory.FactoryInterface
     interface AssemblyPlant
     {
         Chassis CreateChassis();
-        Body CraeteBody();
+        Body CreateBody();
         Engine CreateEngine();
     }
+    class ModelYAssemblyPlant : AssemblyPlant
+    {
+        public Chassis CreateChassis() { return new ModelYChassis(); }
+        public Body CreateBody() { return new ModelYBody(); }
+        public Engine CreateEngine() { return new ModelYEngine(); }
+
+
+    }
+    class ModelSAssemblyPlant : AssemblyPlant
+    {
+        public Chassis CreateChassis() { return new ModelSChassis(); }
+        public Body CreateBody() { return new ModelSBody(); }
+        public Engine CreateEngine() { return new ModelSEngine(); }
+
+    }
+
 }
